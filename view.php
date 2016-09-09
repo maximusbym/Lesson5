df898aa045c5da2cc9ec24124d6ffc59361f5dc6
Max <kalagur11@gmail.com>
2016-09-09 11:22:22 +0300

    homework

--- view.php
+++ view.php
@@ -7,19 +7,3 @@ foreach( $newMessages as $val ) {
 	echo '<hr/>';
 }
 
-//You can even add more Dollar Signs
-
-  $Bar = "a";
-  $Foo = "Bar";
-  $World = "Foo";
-  $Hello = "World";
-  $a = "Hello";
-
-  echo $a; //Returns Hello
-  echo ${$a}; //Returns World
-  echo $$$a; //Returns Foo
-  echo $$$$a; //Returns Bar
-  echo $$$$$a; //Returns a
-
-  echo $$$$$$a; //Returns Hello
-
